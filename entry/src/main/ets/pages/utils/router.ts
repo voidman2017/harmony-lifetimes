1959b971d5a0bdb2e9c17e2634e6dce2c0ed563d
princed <1138322908@qq.com>
2023-12-27 18:26:56 +0800

    init

---
+++ entry/src/main/ets/pages/utils/router.ts
@@ -0,0 +1,26 @@
+import router from '@ohos.router';
+import hilog from '@ohos.hilog';
+
+// 使用类型别名来简化函数类型的定义
+type PushUrlFunction = (option: router.RouterOptions, mode?: router.RouterMode) => void;
+type BackFunction = (option?: router.RouterOptions) => void
+
+interface IRoute {
+  pushUrl: PushUrlFunction
+  back: BackFunction
+}
+
+export const route: IRoute = {
+  pushUrl: (option: router.RouterOptions, mode: router.RouterMode = router.RouterMode.Standard) => {
+    const routerState: router.RouterState = router.getState()
+    // 将枚举的值转换为对应的键
+    const modeKey = Object.keys(router.RouterMode)[Object.values(router.RouterMode).indexOf(mode)];
+    hilog.info(0x0000, 'testTag', '%{public}s', `${routerState.path}${routerState.name} ------pushUrl-------> ${option.url}  [${modeKey}]`);
+    router.pushUrl(option)
+  },
+  back: (option?: router.RouterOptions) => {
+    const routerState: router.RouterState = router.getState()
+    hilog.info(0x0000, 'testTag', '%{public}s', `${option?.url ? option.url : ''} <------back------- ${routerState.path}${routerState.name}`);
+    router.back(option)
+  }
+}
