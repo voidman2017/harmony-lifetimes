407542ea70af883388111ad312518fb7a2753c72
princed <1138322908@qq.com>
2023-12-28 10:40:25 +0800

    init

--- entry/src/main/ets/pages/utils/router.ts
+++ entry/src/main/ets/pages/utils/router.ts
@@ -4,10 +4,12 @@ import hilog from '@ohos.hilog';
 // 使用类型别名来简化函数类型的定义
 type PushUrlFunction = (option: router.RouterOptions, mode?: router.RouterMode) => void;
 type BackFunction = (option?: router.RouterOptions) => void
+type replaceUrlFunction = (option: router.RouterOptions, mode?: router.RouterMode) => void;
 
 interface IRoute {
   pushUrl: PushUrlFunction
   back: BackFunction
+  replaceUrl: replaceUrlFunction
 }
 
 export const route: IRoute = {
@@ -22,5 +24,12 @@ export const route: IRoute = {
     const routerState: router.RouterState = router.getState()
     hilog.info(0x0000, 'testTag', '%{public}s', `${option?.url ? option.url : ''} <------back------- ${routerState.path}${routerState.name}`);
     router.back(option)
-  }
+  },
+  replaceUrl: (option: router.RouterOptions, mode: router.RouterMode = router.RouterMode.Standard) => {
+    const routerState: router.RouterState = router.getState()
+    // 将枚举的值转换为对应的键
+    const modeKey = Object.keys(router.RouterMode)[Object.values(router.RouterMode).indexOf(mode)];
+    hilog.info(0x0000, 'testTag', '%{public}s', `${routerState.path}${routerState.name} ------replaceUrl-------> ${option.url}  [${modeKey}]`);
+    router.replaceUrl(option)
+  },
 }
